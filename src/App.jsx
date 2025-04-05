16e7ab4b4ea2b902282fbb05b654b9640961de43
dev <popeopzy@gmail.com>
2025-04-05 14:25:42 +0100

    Commiting this without landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
+import {ColorModeContext,useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import TopBar from './scenes/global/TopBar'
+import LandingPage from './components/landing_page/LandingPage'
+import DashboardIndex from './scenes/DashboardIndex'
+// import SideBar from './scenes/global/SideBar'
+// import Team from './scenes/Team'
+// import Contacts from './scenes/Contacts'
+// import Invoices from './scenes/Invoices'
+// import Form from './scenes/Form'
+// import Calendar from './scenes/Calendar'
+// import FAQ from './scenes/FAQ'
+// import Bar from './scenes/bar'
+// import Line from './scenes/Line'
+// import Fag from './scenes/Fag'
 
 function App() {
+  const [theme, colorMode] = useMode()
   return (
-    <div>App</div>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+          <div className="app">
+              <main className='content'>
+                {/* <TopBar/> */}
+                <Routes>
+
+                  <Route path="/" element={<LandingPage/>}></Route>
+                </Routes>
+              </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
   )
 }
 
